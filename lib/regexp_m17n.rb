8a860495629f4d19f3d44d2c25af757593204636
Andrew Vit <andrew@avit.ca>
2014-08-02 00:56:44 -0700

    Cascade Regex failures to retry different encoding methods
    If a string can be directly transcoded to UTF-8 then try to do so first.
    Failing that, compare the string as a BINARY encoded string.

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,13 @@
 module RegexpM17N
   def self.non_empty?(str)
     str =~ /^.+$/
+  rescue Encoding::CompatibilityError
+    str = str.dup
+    begin
+      str.encode!(Encoding::UTF_8)
+    rescue Encoding::ConverterNotFoundError
+      str.force_encoding(Encoding::BINARY)
+    end
+    retry
   end
 end
