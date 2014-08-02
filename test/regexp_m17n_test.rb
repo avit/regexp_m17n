3ab97ffbb25728dbb0a415be3d04173652d41baf
Andrew Vit <andrew@avit.ca>
2014-08-01 22:40:39 -0700

    Define tests for individual encoding targets

--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -2,9 +2,9 @@
 require 'minitest/autorun'
 require_relative '../lib/regexp_m17n'
 
-class RegexpTest < MiniTest::Unit::TestCase
-  def test_non_empty_string
-    Encoding.list.each do |enc|
+class RegexpTest < MiniTest::Test
+  Encoding.list.each do |enc|
+    define_method "test_non_empty_#{enc.name.downcase.gsub(/[^a-z0-9]/, '_')}" do
       assert(RegexpM17N.non_empty?('.'.encode(enc)))
     end
   end
